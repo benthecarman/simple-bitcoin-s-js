c83e5c2b2ebf10068e8875498d1b3b52ce1f688b
Chris Stewart <stewart.chris1234@gmail.com>
2021-04-04 09:35:44 -0500

    Add new image, fix printing address

--- src/main/scala/org/bitcoins/simple/BitcoinSExample.scala
+++ src/main/scala/org/bitcoins/simple/BitcoinSExample.scala
@@ -1,9 +1,11 @@
 package org.bitcoins.simple
 import org.bitcoins.core.config.TestNet3
+import org.bitcoins.core.protocol.{Bech32Address, P2PKHAddress}
 import org.bitcoins.core.protocol.script.P2WPKHWitnessSPKV0
 import org.bitcoins.crypto.ECPrivateKey
 import org.scalajs.dom
 import org.scalajs.dom.document
+
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object BitcoinSExample {
@@ -14,13 +16,13 @@ object BitcoinSExample {
     val publicKey =  privKey.publicKey
     val network = TestNet3
     val p2wpkh = P2WPKHWitnessSPKV0(publicKey)
-    //val addr = P2PKHAddress.fromScriptPubKey(p2wpkh,network)
+    val addr = Bech32Address.fromScriptPubKey(p2wpkh,network)
     appendPar(document.body, s"privKey=${privKey.hex}")
     appendPar(document.body, s"pubKey=${publicKey.hex}")
     appendPar(document.body, s"p2wpkh=${p2wpkh.asmBytes}")
     appendPar(document.body, s"network=$network")
-    //(document.body,s"addr=${addr.toString()}")
-
+    appendPar(document.body,s"addr=${addr.toString()}")
+    ()
   }
 
   def appendPar(targetNode: dom.Node, text: String): Unit = {
@@ -36,12 +38,12 @@ object BitcoinSExample {
     val publicKey =  privKey.publicKey
     val network = TestNet3
     val p2wpkh = P2WPKHWitnessSPKV0(publicKey)
-    //val addr = P2PKHAddress(p2wpkh,network)
+    val addr = Bech32Address.fromScriptPubKey(p2wpkh,network)
     appendPar(document.body, s"privKey=${privKey.hex}")
     appendPar(document.body, s"pubKey=${publicKey.hex}")
     appendPar(document.body, s"p2wpkh=${p2wpkh.asmBytes}")
     appendPar(document.body, s"network=$network")
-    //appendPar(document.body,s"addr=${addr.toString()}")
+    appendPar(document.body,s"addr=${addr.toString()}")
   }
 
 }
