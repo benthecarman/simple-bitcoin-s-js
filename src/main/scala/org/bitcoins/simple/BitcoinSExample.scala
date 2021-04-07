47fb541a773e3497c97dab077dbeb0433186da9d
Ben Carman <benthecarman@live.com>
2021-04-07 14:07:04 -0500

    Selector for contract type

--- src/main/scala/org/bitcoins/simple/BitcoinSExample.scala
+++ src/main/scala/org/bitcoins/simple/BitcoinSExample.scala
@@ -1,10 +1,10 @@
 package org.bitcoins.simple
 
 import org.bitcoins.core.currency.Satoshis
-import org.bitcoins.core.protocol.dlc.{CallOption, RoundingIntervals}
+import org.bitcoins.core.protocol.dlc._
 import org.scalajs.dom
-import org.scalajs.dom.html.Input
-import org.scalajs.dom.{document, Element}
+import org.scalajs.dom.html.{Input, Select}
+import org.scalajs.dom.{document, html, Element}
 
 import scala.scalajs.js.annotation.JSExportTopLevel
 
@@ -12,7 +12,21 @@ object BitcoinSExample {
 
   def main(args: Array[String]): Unit = {
     appendPar(document.body, "DLC Templates")
-    appendPar(document.body, "Call Option")
+
+    val contractSelector = document.createElement("select").asInstanceOf[Select]
+
+    val optionStrings =
+      Vector("Call Option", "Put Option", "Long CFD", "Short CFD")
+
+    optionStrings.foreach { str =>
+      val elem = document.createElement("option").asInstanceOf[html.Option]
+      elem.textContent = str
+      contractSelector.add(elem)
+    }
+
+    document.body.appendChild(contractSelector)
+    document.body.appendChild(document.createElement("br"))
+    document.body.appendChild(document.createElement("br"))
 
     val individualCollateral = document.createElement("label")
     individualCollateral.textContent = "Individual Collateral"
@@ -31,7 +45,7 @@ object BitcoinSExample {
 
     val premium = document.createElement("label")
     premium.textContent = "Premium"
-    val premiumText = document.createElement("input")
+    val premiumText = document.createElement("input").asInstanceOf[Input]
     document.body.appendChild(premium)
     document.body.appendChild(premiumText)
     document.body.appendChild(document.createElement("br"))
@@ -56,14 +70,29 @@ object BitcoinSExample {
     val create = document.createElement("button")
     create.textContent = "Create Descriptor"
 
+    contractSelector.addEventListener(
+      "click",
+      { (_: dom.MouseEvent) =>
+        contractSelector.value match {
+          case "Call Option" | "Put Option" =>
+            premiumText.disabled = false
+          case "Long CFD" | "Short CFD" =>
+            premiumText.disabled = true
+          case _ =>
+            throw new RuntimeException("This is impossible")
+        }
+      }
+    )
+
     //when the button is clicked call the generateAddress function
     create.addEventListener(
       "click",
       { (_: dom.MouseEvent) =>
         generateDescriptor(
+          contractSelector = contractSelector,
           individualCollateralText = individualCollateralText,
           totalCollateralText = totalCollateralText.asInstanceOf[Input],
-          premiumText = premiumText.asInstanceOf[Input],
+          premiumText = premiumText,
           numDigitsText = numDigitsText.asInstanceOf[Input],
           strikePriceText = strikePriceText.asInstanceOf[Input],
           resultArea = resultArea
@@ -86,6 +115,7 @@ object BitcoinSExample {
 
   @JSExportTopLevel("generateDescriptor")
   def generateDescriptor(
+      contractSelector: Select,
       individualCollateralText: Input,
       totalCollateralText: Input,
       premiumText: Input,
@@ -93,18 +123,42 @@ object BitcoinSExample {
       strikePriceText: Input,
       resultArea: Element): Unit = {
     println("generateDescriptor")
+
     val individualCollateral = Satoshis(individualCollateralText.value.toLong)
     val totalCollateral = Satoshis(totalCollateralText.value.toLong)
     val premium = Satoshis(premiumText.value.toLong)
     val numDigits = numDigitsText.value.toInt
     val strikePrice = strikePriceText.value.toLong
 
-    val template: CallOption = CallOption(individualCollateral,
-                              totalCollateral,
-                              numDigits,
-                              strikePrice,
-                              premium,
-                              RoundingIntervals.noRounding)
+    val template = contractSelector.value match {
+      case "Call Option" =>
+        CallOption(individualCollateral,
+                   totalCollateral,
+                   numDigits,
+                   strikePrice,
+                   premium,
+                   RoundingIntervals.noRounding)
+      case "Put Option" =>
+        PutOption(individualCollateral,
+                  totalCollateral,
+                  numDigits,
+                  strikePrice,
+                  premium,
+                  RoundingIntervals.noRounding)
+      case "Long CFD" =>
+        LongCFD(individualCollateral,
+                totalCollateral,
+                numDigits,
+                strikePrice,
+                RoundingIntervals.noRounding)
+      case "Short CFD" =>
+        LongCFD(individualCollateral,
+                totalCollateral,
+                numDigits,
+                strikePrice,
+                RoundingIntervals.noRounding)
+      case _ => throw new RuntimeException("This is impossible")
+    }
 
     println(template.toContractDescriptor.hex)
 
